2492220fb4b13a0a9de8f22988b587f3a818307e
martingb04 <martingomezbarral3@gmail.com>
2024-12-17 02:43:35 +0100

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    res.end('¡Hola, Mundo!');
-});
-
-server.listen(3000, () => {
-    console.log('Servidor funcionando en http://localhost:3000');
-});
